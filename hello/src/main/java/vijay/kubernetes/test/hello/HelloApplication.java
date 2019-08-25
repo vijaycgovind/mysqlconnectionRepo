a970656bf57022478802589154c1ea1d36d4342a
Vijay <vijucg@yahoo.com>
2019-08-25 10:47:52 +0530

    checkin

---
+++ hello/src/main/java/vijay/kubernetes/test/hello/HelloApplication.java
@@ -0,0 +1,25 @@
+package vijay.kubernetes.test.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/hello")
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+		System.out.println("Hello Kubernetes Versino 2 -----upgraded");
+	}
+
+		@GetMapping
+		public String hello()
+		{
+			return "HELLO FROM KUBERNETERs***************UPGRADED************>>>>>";
+		}
+	
+}
---
+++ hello/src/test/java/vijay/kubernetes/test/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package vijay.kubernetes.test.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
